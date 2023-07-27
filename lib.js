e88ad2ad2da0406c949ac0d6a4e9a540e60c7bd0
Cristian Mazzetti Martin <cristian@kalexgroup.com>
2023-07-27 16:16:21 +0200

    update auth in

--- lib.js
+++ lib.js
@@ -11,7 +11,7 @@ function getCookie(name) {
 
 function profile() {
     let profile = document.getElementById("profile");
-    if (getCookie('kid') !== undefined ){
+    if (getCookie('spertoj_token_id') !== undefined ){
         profile.innerHTML = "<span id=\"logout\" class=\"material-symbols-outlined material-b\" onclick=\"\">menu</span>";
     } else {
         profile.innerHTML = "<span id=\"login\" class=\"material-symbols-outlined material-b\" onclick=\"window.open('https://login.spertoj.com/login?client_id=4bldpvs0sk1u0m6km6basfoms&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fspertoj.com%2Fauth%2Fin.html', '_self');\">login</span>";
