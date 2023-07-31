be8065317f1303a57e246ae1477e43656bf5ee59
Cristian Mazzetti Martin <cristian@kalexgroup.com>
2023-07-31 23:04:38 +0200

    update main js

--- main.js
+++ main.js
@@ -9,17 +9,25 @@ profile();
 //  GIS
 //
 
-new Map({
-    target: 'map',
+var attribution = new ol.control.Attribution({
+    collapsible: false
+});
+
+var map = new ol.Map({
+    controls: ol.control.defaults({attribution: false}).extend([attribution]),
     layers: [
-        new TileLayer({
-            source: new XYZ({
-                url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+        new ol.layer.Tile({
+            source: new ol.source.OSM({
+                url: 'https://tile.openstreetmap.be/osmbe/{z}/{x}/{y}.png',
+                attributions: [ ol.source.OSM.ATTRIBUTION, 'Tiles courtesy of <a href="https://geo6.be/">GEO-6</a>' ],
+                maxZoom: 18
             })
         })
     ],
-    view: new View({
-        center: [0, 0],
-        zoom: 2
+    target: 'map',
+    view: new ol.View({
+        center: ol.proj.fromLonLat([4.35247, 50.84673]),
+        maxZoom: 18,
+        zoom: 12
     })
 });
