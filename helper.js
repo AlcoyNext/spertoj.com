0f14f32c893fe8abd6971ba1f398a80e127133ce
Cristian Mazzetti Martin <cristian@kalexgroup.com>
2023-07-31 22:57:47 +0200

    update openlayers

--- helper.js
+++ helper.js
@@ -17,34 +17,3 @@ function profile() {
         profile.innerHTML = "<span id=\"login\" class=\"material-symbols-outlined material-b\" onclick=\"window.open('https://login.spertoj.com/login?client_id=4bldpvs0sk1u0m6km6basfoms&response_type=token&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fspertoj.com%2Fauth%2Fin.html', '_self');\">login</span>";
     }
 }
-
-
-// 
-//  GIS
-//
-function checkControl(control, div) {
-    if (map.hasControl(control)) {
-        map.removeControl(control);
-        div.classList.remove("active");
-    } else {
-        map.addControl(
-            control,
-            'top-right'
-        );
-        div.classList.add("active");
-    }
-}
-
-/* LAYERS */
-function layerPop(layer) {
-    let element = document.getElementById(layer);
-    let x = element.offsetLeft;
-    let y = element.offsetTop;
-
-
-}
-
-function checkLayer() {
-
-}
-
--- main.js
+++ main.js
@@ -9,36 +9,17 @@ profile();
 //  GIS
 //
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWxjb3luZXh0IiwiYSI6ImNsanNhaWg5OTA0cnQzZnA5bjA2b2ZkNHYifQ.MWQzPqXVPqt3Kj0LxIWBdQ';
-// PRODUCTION
-//mapboxgl.accessToken = 'pk.eyJ1IjoiYWxjb3luZXh0IiwiYSI6ImNsanNiaHdhejA4eDMzZm4weGhqYTRnaHcifQ.37crDNKv4geComo0kCPQag';
-
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/alcoynext/cljsfzfr9019401p596srfj11',
-    center: [-0.47298, 38.69826],
-    zoom: 12,
-    language: 'es'
-});
-
-const control_navigation = new mapboxgl.NavigationControl();
-map.addControl(control_navigation);
-
-const control_geolocate = new mapboxgl.GeolocateControl({
-    positionOptions: {
-        enableHighAccuracy: true
-    },
-    trackUserLocation: true,
-    showUserHeading: true
-});
-map.addControl(control_geolocate);
-
-
-
-map.on('load', () => {
-
-});
-
-map.on('idle', () => {
-
+new Map({
+    target: 'map',
+    layers: [
+        new TileLayer({
+            source: new XYZ({
+                url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        })
+    ],
+    view: new View({
+        center: [0, 0],
+        zoom: 2
+    })
 });
