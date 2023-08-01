51d72e08b16dedb9719e49c2f1b26652c32c9a76
Cristian Mazzetti Martin <cristian@kalexgroup.com>
2023-08-01 06:44:52 +0200

    update TomTom init

--- helper.js
+++ helper.js
@@ -17,3 +17,31 @@ function profile() {
         profile.innerHTML = "<span id=\"login\" class=\"material-symbols-outlined material-b\" onclick=\"window.open('https://login.spertoj.com/login?client_id=4bldpvs0sk1u0m6km6basfoms&response_type=token&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fspertoj.com%2Fauth%2Fin.html', '_self');\">login</span>";
     }
 }
+
+// 
+//  GIS
+//
+
+function gisInit() {
+    tt.setProductInfo("spertoj.com", "1")
+    let map = tt.map({
+        key: "idrSO8HEhlajaGPSMkSAszNR9lTMCMHQ",
+        container: "map",
+        center: [-0.47298, 38.69826],
+        zoom: 11,
+    })
+
+    let attributions = ['<a href="https://erp.alcoynext.com/helpdesk" target="_blank">ALCOY NEXT Inc.</a>'];
+    map.getAttributionControl().addAttribution(attributions);
+
+    let nav = new tt.NavigationControl({});
+    map.addControl(nav, 'top-right');
+
+    map.addControl(new tt.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        trackUserLocation: true
+    }));
+    return map;
+}
--- main.js
+++ main.js
@@ -9,11 +9,4 @@ profile();
 //  GIS
 //
 
-tt.setProductInfo("spertoj.com", "1")
-let map = tt.map({
-    key: "idrSO8HEhlajaGPSMkSAszNR9lTMCMHQ",
-    container: "map",
-})
-
-var nav = new tt.NavigationControl({});
-map.addControl(nav, 'top-left');
+var map = gisInit();
